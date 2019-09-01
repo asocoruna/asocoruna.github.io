64ad932811d43ce828fd425271cf3e162819b041
TretornESP <damebandakl@gmail.com>
2019-09-01 22:32:12 +0200

    Delete gtag.js

--- gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-146762892-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-146762892-1');
-</script>
